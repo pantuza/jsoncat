d905c0c042ca9a27231fcbf142c04a8a45fde867
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-09-15 00:57:13 -0300

    Adding the jsoncat C file (No code for the moment)

---
+++ src/jsoncat.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  jsoncat.c
+ *
+ *    Description:  This file is the main execution of the jsoncat program
+ *
+ *        Version:  1.0
+ *        Created:  09/15/2015 12:46:34 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gustavo Pantuza 
+ *   Organization:  Software Community
+ *
+ * =====================================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int
+main (int argc, char* argv[])
+{
+
+    return EXIT_SUCCESS;
+}
