baecaccfe4f83954d8065c459251feb96b418b7f
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-09-15 19:49:22 -0300

    Created definitions of the ANSI colors to pretty-print json objects

---
+++ src/colors.h
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  colors.h
+ *
+ *    Description:  This file define the output colors used by the program
+ *
+ *        Version:  1.0
+ *        Created:  09/15/2015 07:17:40 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gustavo Pantuza, 
+ *   Organization:  Software Community
+ *
+ * =====================================================================================
+ */
+
+#ifndef JSONCAT_COLORS_H
+#define JSONCAT_COLORS_H
+
+
+/**
+ *
+ * ANSI color definitions that pretty-print the json object
+ *
+ */
+
+#define NO_COLOR    "\x1b[0m"
+#define RED         "\x1b[31m"
+#define BLUE        "\x1b[34m"
+#define GREEN       "\x1b[32m"
+#define BROWN       "\x1b[33m"
+#define GRAY        "\x1b[37m"
+
+
+#endif // JSONCAT_COLORS_H
