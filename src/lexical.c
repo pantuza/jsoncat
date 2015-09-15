5770620bc57bb0a6bbe21252d8837b2cc09bbb79
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-09-15 19:58:07 -0300

    Created files for the lexical analysis

---
+++ src/lexical.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lexical.c
+ *
+ *    Description:  This file is responsible of identify tokens of the json
+ *                  object model
+ *
+ *        Version:  1.0
+ *        Created:  09/15/2015 07:52:44 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gustavo Pantuza, 
+ *   Organization:  Software Community
+ *
+ * =====================================================================================
+ */
+
+
+#include "lexical.h"
---
+++ src/lexical.h
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lexical.h
+ *
+ *    Description:  Header file of the lexical analyser program
+ *
+ *        Version:  1.0
+ *        Created:  09/15/2015 07:54:52 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gustavo Pantuza, 
+ *   Organization:  Software Community
+ *
+ * =====================================================================================
+ */
+
+#ifndef LEXICAL_H
+#define LEXICAL_H
+
+
+
+#endif // LEXICAL_H
