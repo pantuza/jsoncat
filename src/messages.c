b91fe57c9ebaceb8e26ecf3430a567fd7c748a74
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-09-17 20:31:50 -0300

    Created message functions of jsoncat

--- src/messages.c
+++ src/messages.c
@@ -25,6 +25,7 @@
 #include "messages.h"
 
 
+
 /*
  * Jsoncat help message
  *
@@ -33,6 +34,11 @@ void
 help ()
 {
     fprintf(stdout, BLUE "J S O N C A T \n\n" NO_COLOR);
+    usage();
+    description();
+    options();
+    author();
+    version();
 }
 
 
@@ -45,7 +51,53 @@ void
 usage ()
 {
     fprintf(stdout, BROWN "Usage: " NO_COLOR);
-    fprintf(stdout, "jsoncat [options] input file\n");
+    fprintf(stdout, "%s [options] input file\n\n", __JSONCAT_PROGRAM_NAME__);
+}
+
+
+
+/* 
+ * Jsoncat description message
+ */
+void
+description ()
+{
+    fprintf(stdout, BROWN "Description: " NO_COLOR);
+    fprintf(stdout, "Json pretty-print parser based on a recursive "
+            "lexical analyser.\n\tThe input file is parsed to build a json "
+            "object. If the object \n\tis correct, it will be "
+            "pretty-printed to standard output\n\n");
+}
+
+
+
+/*
+ * Jsoncat options message
+ */
+void
+options ()
+{
+    fprintf(stdout, BROWN "Options:\n\n" NO_COLOR);
+    fprintf(stdout, GRAY "\t-v|--version\n" NO_COLOR
+                    "\t\tPrints %s version\n\n", __JSONCAT_PROGRAM_NAME__);
+    fprintf(stdout, GRAY "\t-h|--help\n" NO_COLOR
+                    "\t\tPrints this help message\n\n");
+    fprintf(stdout, GRAY "\t--no-color\n" NO_COLOR
+                    "\t\tDoes not prints json files using colors\n\n");
+
+}
+
+
+
+/* 
+ * Jsoncat author message
+ *
+ */
+void
+author ()
+{
+    fprintf(stdout, BROWN "Written by: " GRAY "%s\n\n" NO_COLOR,
+            __JSONCAT_AUTHOR__);
 }
 
 
@@ -57,5 +109,6 @@ usage ()
 void
 version ()
 {
-    fprintf(stdout, "jsoncat version " BROWN __JSONCAT_VERSION__ NO_COLOR);
+    fprintf(stdout, "jsoncat version " GRAY "%s\n" NO_COLOR,
+            __JSONCAT_VERSION__);
 }
