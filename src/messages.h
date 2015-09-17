da15b6b20c618917c51be78d2535052b721072b1
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-09-17 20:31:45 -0300

    Added public functions and macros to messages header file

--- src/messages.h
+++ src/messages.h
@@ -21,9 +21,17 @@
 #define MESSAGES_H
 
 
+#define __JSONCAT_PROGRAM_NAME__ "jsoncat"
+#define __JSONCAT_VERSION__ "0.1"
+#define __JSONCAT_AUTHOR__ "Gustavo Pantuza (gustavopantuza@gmail.com)"
+
+
 void help ();
 void usage ();
 void version ();
+void description ();
+void options ();
+void author ();
 
 
 #endif // MESSAGES_H
