2e9103da477550010c4c67927505d5f9bc8cb28a
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-09-27 02:53:46 -0300

    Created file for token handle

---
+++ src/tokens.c
@@ -0,0 +1,20 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  tokens.c
+ *
+ *    Description:  Functions that controls the token
+ *
+ *        Version:  1.0
+ *        Created:  09/27/2015 02:23:31 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gustavo Pantuza
+ *   Organization:  Software Community
+ *
+ * ============================================================================
+ */
+
+
+#include "tokens.h"
