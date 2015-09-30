fcd216e74f7d3c332396de25303ca2f06c535884
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-09-30 00:25:49 -0300

    Defined a generic string token and public token functions

--- src/tokens.h
+++ src/tokens.h
@@ -32,16 +32,18 @@
 #define VALUE_SEPARATOR ','
 #define PAIR_SEPARATOR  ':'
 
+#define STRING_TOKEN    900
 #define STRING_0        '\''
 #define STRING_1        '"'
 #define STRING_SCAPE    '\\'
 
-#define TRUE_VALUE      900
-#define FALSE_VALUE     901
-#define NULL_VALUE      902
+#define TRUE_VALUE      901
+#define FALSE_VALUE     902
+#define NULL_VALUE      903
 
-#define NUMBER          903
+#define NUMBER          904
 
+#define TOKEN_ERROR     905
 
 
 /* Considered size of any color string defined in colors.h */
@@ -72,6 +74,12 @@ struct token
 
 
 /* Public function to handle token struct */
+
+void inc_line(struct token *token, int inc_size);
+void inc_column (struct token *token, int inc_size);
+void set_color (struct token *token, char color[COLOR_STR_SIZE]);
+void set_value (struct token *token, char value[DEFAULT_VALUE_LENGTH]);
+
 
 void update_token (struct token *token, int type, char color[COLOR_STR_SIZE],
               char value[DEFAULT_VALUE_LENGTH], int inc_line, int inc_column);
