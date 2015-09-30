b9f204034cbb0224cbe55ddfa24f7601cc837023
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-09-30 00:26:33 -0300

    Created separated functions for each struct token field

--- src/tokens.c
+++ src/tokens.c
@@ -19,6 +19,51 @@
 
 #include "tokens.h"
 #include "colors.h"
+#include "string.h"
+
+
+
+/*
+ * Increment the token line
+ */
+void
+inc_line(struct token *token, int inc_size)
+{
+    token->line += (inc_size) ? inc_size : 0;
+}
+
+
+
+/*
+ * Increment the token column
+ */
+void
+inc_column (struct token *token, int inc_size)
+{
+    token->column += (inc_size) ? inc_size : 0;
+}
+
+
+
+/*
+ * Sets the token value color
+ */
+void
+set_color (struct token *token, char color[COLOR_STR_SIZE])
+{
+    strncpy(token->color, color, COLOR_STR_SIZE);
+}
+
+
+
+/*
+ * Sets the token value
+ */
+void
+set_value (struct token *token, char value[DEFAULT_VALUE_LENGTH])
+{
+    strncpy(token->value, value, DEFAULT_VALUE_LENGTH);
+}
 
 
 
@@ -27,16 +72,13 @@
  */
 void
 update_token (struct token *token, int type, char color[COLOR_STR_SIZE],
-              char value[DEFAULT_VALUE_LENGTH], int inc_line, int inc_column)
+              char value[DEFAULT_VALUE_LENGTH], int inc_l, int inc_c)
 {
-
     token->type = type;
 
-    token->line += (inc_line) ? inc_line : 0;
-
-    token->column += (inc_column) ? inc_column : 0;
-
-    strncpy(token->color, RED, COLOR_STR_SIZE);
-
-    strncpy(token->value, value, DEFAULT_VALUE_LENGTH);
+    inc_line(token, inc_l);
+    inc_column(token, inc_c);
+    
+    set_color(token, color);
+    set_value(token, value);
 }
