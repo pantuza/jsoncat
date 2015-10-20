2b9ec0c6e94e7d1a02b973de4ee44dc5136f6736
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-10-20 00:36:27 -0200

    Renamed the Command line options test script

---
+++ tests/command_args.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+#
+# This bash script verifies that command line arguments are working right
+#
+
+
+# Include tests utils
+. tests/test_utils.sh
+
+
+# -------------------- T E S T S ------------------------ #
+
+
+# Check binary
+binary_exists;
+
+
+# Prints start message
+start_message "Command line options";
+
+
+#
+# Verifies -v command line options
+#
+test_message "Prints program version with -v option";
+./${BINDIR}/${BINARY} -v | grep "version" &> ${LIMBO};
+assert_command $?;
+
+
+
+#
+# Verifies --version command line option
+#
+test_message "Prints program version with --version option";
+./${BINDIR}/${BINARY} --version | grep "version" &> ${LIMBO};
+assert_command $?;
+
+
+
+#
+# Verifies -h command line option
+#
+test_message "Prints program help with -h option";
+./${BINDIR}/${BINARY} -h | grep "Description" &> ${LIMBO};
+assert_command $?;
+
+
+
+#
+# Verifies --help command line option
+#
+test_message "Prints program help with --help option";
+./${BINDIR}/${BINARY} --help | grep "Description" &> ${LIMBO};
+assert_command $?;
+
+
+
+#
+# Verifies the use of stdin as the input json
+#
+test_message "Use stdin as the input json to parse";
+echo "{'test': true}" | ${BINDIR}/${BINARY} &> ${LIMBO};
+assert_command $?;
+
+
+
+#
+# Tests no such file or directory
+#
+test_message "Validate if file does not exists";
+./${BINDIR}/${BINARY} "some_invalid_file_name.json" &> ${LIMBO};
+assert_command_fail $?;
+
+
+
+#
+# Tests a valid json input file
+#
+test_message "Parse a valid json input file";
+./${BINDIR}/${BINARY} "tests/samples/input_06.json" &> ${LIMBO};
+assert_command $?;
+
+
+# Prints tests result
+result_message;
