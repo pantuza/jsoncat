71877e8bdec62fac92078234801b56be8f24063e
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-10-22 00:33:15 -0200

    Created the strings test script

---
+++ tests/strings.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#
+# This bash script parses different kinds of strings
+#
+
+
+# Include tests utils
+. tests/utils.sh
+
+
+# -------------------- T E S T S ------------------------ #
+
+
+# Check binary
+binary_exists;
+
+
+# Prints start message
+start_message "Parsing strings";
+
+
+#
+# Tests an empty string
+#
+STR="''";
+test_message "Parsing an empty string";
+has_str "" $STR;
+assert_command $?;
+
+#
+# Tests a string with numbers
+#
+STR="42";
+test_message "Parsing a string with a number";
+has_str "42" $STR;
+assert_command $?;
+
+
+#
+# Tests a string with an object
+#
+STR='{"a":10}';
+test_message "Parsing a string with an object";
+has_str '{"a":10}' $STR;
+assert_command $?;
+
+
+
+#
+# Tests a string with an array
+#
+STR="\[10, 12.2, 23\]";
+test_message "Parsing a string with an array";
+has_str "[10, 12.2, 23]" $STR;
+assert_command $?;
+
+
+
+# Prints tests result
+result_message;
