9479e99e38eabf65e3195a275714145fc02d6f76
{NAME} <gustavopantuza@gmail.com>
2015-11-28 20:17:34 -0200

    Default bash variables for scripting

---
+++ build/scripts/utils.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+ECHO=$(which echo);
+ECHOOPTS="-en";
+
+CAT=$(which cat);
+
+GREP=$(which grep);
+
+CUT=$(which cut);
+
+SED=$(which sed);
+SEDOPTS="-i";
+
+GIT=$(which git);
+
+
+# Files
+VERSION_FILE="../src/messages.h";
